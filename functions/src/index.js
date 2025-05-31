640f8070c1a673b25e5a3d5423d2deffe47800ef
farleymrtnz25 <frlmrtnz@gmail.com>
2025-05-31 12:10:16 -0500

    Mensaje explicativo de los cambios realizados

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const trabajadoresRoutes = require('./routes/trabajadores');
-const inventarioRoutes = require('./routes/inventario');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/.netlify/functions/api/trabajadores', trabajadoresRoutes);
-app.use('/.netlify/functions/api/inventario', inventarioRoutes);
+app.use('/trabajadores', trabajadoresRoutes);
 
 module.exports.handler = serverless(app);
